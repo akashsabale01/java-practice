3e658224a936cb2aa5ceaec1b16ce62c1b463462
Akash Sabale <akashsabale100@gmail.com>
2023-05-09 18:01:12 +0530

    Added code in HashMapDemo

--- 20_Collections/HashMapDemo/src/Main.java
+++ 20_Collections/HashMapDemo/src/Main.java
@@ -42,6 +42,7 @@ public class Main {
         /*
             System.out.println(countries.entrySet()); is same as System.out.println(countries);
         */
+        
         // print
         System.out.println(countries);
 
